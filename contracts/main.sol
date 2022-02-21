d0c47145621ab68e61efda9175510aeddcda6ac8
Calvin Joshua <calvin.joshua@thefastway.in>
2022-02-21 18:14:04 +0530

    second commit

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract main{
 
     IERC20 public token;
@@ -11,33 +10,35 @@ contract main{
     constructor(address _token) {
         token = IERC20(_token);
         owner = payable(msg.sender);
-
     }
 
-    function transfer(address payable con, address payable to, uint256 amt) internal {
-        bool sent = con.send(msg.value);
-        require(sent, "Failed to send Ether");
+    function transfer(address payable to, uint256 amt) internal {
+        bool sent = owner.send(msg.value);
+        require(sent, "Failed to transfer Ether");
         token.transfer(to, amt);
     }
 
-    //Assuming tokens are transfered to the current(main.sol) contract, so inital tokens are transffered and contained in this contract (main.sol)
-    //converted 0.01$ => ether, which is then represented in wei 
-    function purchase(uint256 tokens) public payable{ //tokens to be mentioned in decimals of 10 power 18(Wei format)
+    function purchase() public payable { //tokens to be mentioned in decimals of 10 power 18(Wei format)
         uint256 supply = token.totalSupply();
         uint256 totalAvailable = token.balanceOf(address(this)) ; 
         require(totalAvailable > 0, "Sold out");
+        require(msg.value > 0, "0 INPUT");
         if(totalAvailable > (70000000000000000000 % supply )){
-            require(msg.value == (tokens * 3600000000000), "not enought amount"); //0.0000036//inWei
-            transfer(payable(owner),payable(msg.sender), tokens);
+            uint256 tokens = msg.value / 3600000000000; //0.0000036 in wei
+            transfer(payable(msg.sender), tokens);
         }
         else if(totalAvailable < (70000000000000000000 % supply ) && totalAvailable >= (20000000000000000000 % supply )){ 
-            require(msg.value == (tokens * 7300000000000), "not enought amount"); //0.0000073//inWei
-            transfer(payable(owner),payable(msg.sender), tokens);
+            uint256 tokens = msg.value / 7300000000000; //0.0000073 in wei
+            transfer(payable(msg.sender), tokens);
         } 
         else{
-            uint256 p = (token.totalSupply() - token.balanceOf(address(this))/1000); 
-            require(msg.value == (tokens * (p * 1000000000000000000)), "not enought amount"); //inWei
-            transfer(payable(owner),payable(msg.sender), tokens);
+             /*
+             finding how many tokens have been sold, making them into format of 7 decimals(since intial prices were 0.0000036 and 0.0000073) 
+             converting them into 10 power 18 and calculating number of tokens according to msg.value
+             */
+            uint256 p = (token.totalSupply() - totalAvailable)/10000000;             
+            uint256 tokens = msg.value / (p * 1000000000000000000);
+            transfer(payable(msg.sender), tokens);
         }
     }
 }
