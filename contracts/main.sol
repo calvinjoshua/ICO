3386d7fb6abbcd0587131bc557af663d9a2d62e2
F Calvin Joshua <82115824+calvinjoshua@users.noreply.github.com>
2022-03-12 22:51:38 +0530

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,45 +1,71 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+
 contract main{
 
     IERC20 public token;
     address payable owner;
+    
+    uint256 public phaseOnePrice = 3600000000000; //0.0000036
+    uint256 public CurrentPrice = 7300000000000; //0.0000073 
 
+    uint256 private phaseOneLimit = 70000000000000000000;
+    uint256 private phaseTwoLimit = 20000000000000000000; 
 
     constructor(address _token) {
+        require(_token != address(0), "Invalid Token Address");
         token = IERC20(_token);
         owner = payable(msg.sender);
     }
 
-    function transfer(address payable to, uint256 amt) internal {
-        bool sent = owner.send(msg.value);
+    event TokenPurchase(address buyer, uint256 amount, uint256 tokens);
+
+    function transfer(address payable to, uint256 tokens, uint256 amt) internal {
+        bool sent = owner.send(amt);
         require(sent, "Failed to transfer Ether");
-        token.transfer(to, amt);
+        token.transfer(to, tokens);
+    }
+
+    function purchase() external payable { 
+        uint256  totalAvailable = token.balanceOf(address(this)) ; 
+        
+        uint256 weiAmount = msg.value;
+        uint256 tokens = 0;
+
+        _validate(totalAvailable,msg.sender, weiAmount);
+
+        if(totalAvailable > phaseOneLimit){
+            _validate2(weiAmount, phaseOnePrice);
+            tokens = weiAmount/ phaseOnePrice;
+            transfer(payable(msg.sender), tokens, weiAmount);
+        }
+        else if(totalAvailable < phaseOneLimit && totalAvailable >= phaseTwoLimit){ 
+            _validate2(weiAmount, CurrentPrice);
+            tokens = weiAmount / CurrentPrice;  
+            transfer(payable(msg.sender), tokens, weiAmount);
+        } 
+        /*
+        after 80% of the tokens are sold, on each purchase, the phase two price (i.e CurretPrice) will be adding by value 0.000000000001. Eventually the last token's price will be 0.000027299 
+        */
+        else{
+            CurrentPrice = CurrentPrice + 1000000; //0.000000000001
+            _validate2(weiAmount, CurrentPrice);
+            tokens = weiAmount / CurrentPrice ;
+            transfer(payable(msg.sender), tokens, weiAmount );
+        }
+        
+        emit TokenPurchase(msg.sender, weiAmount, tokens);
+    }
+
+    function _validate(uint256 _totalAvailable,address sender, uint256 amount) internal pure {
+        require(sender != address(0), "Invalid Address");
+        require(_totalAvailable > 0, "Sold out");
+        require(amount != 0, "0 INPUT");
+    }
+
+    function _validate2(uint256 amt, uint256 price) internal pure {
+        require(amt > price, "No enough amount");
     }
     
-    //Assuming tokens are transffered to the contract intially
-    function purchase() public payable { //tokens to be mentioned in decimals of 10 power 18(Wei format)
-        uint256 supply = token.totalSupply();
-        uint256 totalAvailable = token.balanceOf(address(this)) ; 
-        require(totalAvailable > 0, "Sold out");
-        require(msg.value > 0, "0 INPUT");
-        if(totalAvailable > (70000000000000000000 % supply )){
-            uint256 tokens = msg.value / 3600000000000; //0.0000036 in wei
-            transfer(payable(msg.sender), tokens);
-        }
-        else if(totalAvailable < (70000000000000000000 % supply ) && totalAvailable >= (20000000000000000000 % supply )){ 
-            uint256 tokens = msg.value / 7300000000000; //0.0000073 in wei
-            transfer(payable(msg.sender), tokens);
-        } 
-        else{
-             /*
-             finding how many tokens have been sold, making them into format of 7 decimals(since intial prices were 0.0000036 and 0.0000073) 
-             converting them into 10 power 18 and calculating number of tokens according to msg.value
-             */
-            uint256 p = (token.totalSupply() - totalAvailable)/10000000;             
-            uint256 tokens = msg.value / (p * 1000000000000000000);
-            transfer(payable(msg.sender), tokens);
-        }
-    }
 }
